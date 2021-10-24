120989e22edba51838210fd3edfdb46dbb64e8fd
Administrator <Administrator@UTASs-MBP.router>
2021-10-24 19:37:50 -0400

    This is Part 2 Web Calculator

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-var numOne = document.getElementById('num-one');
-var numTwo = document.getElementById('num-two');
-var addSum = document.getElementById('add-sum');
-numOne.addEventListener('input', add);
-numTwo.addEventListener('input', add);
-function add() {
-var one = parseFloat(numOne.value) || 0;
-var two = parseFloat(numTwo.value) || 0;
-addSum.innerText = one+two;
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
 }
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 / num2;
+}
+function add()
+{
+    let num1 = document.getElementById("firstNumber").value;
+    let num2 = document.getElementById("secondNumber").value;
+    document.getElementById("result").innerHTML = parseInt(num1) + parseInt(num2);
+}
+
+function subtract() 
+{
+        const num1 = parseInt(document.getElementById("firstNumber").value);
+        const num2 = parseInt(document.getElementById("secondNumber").value);
+        document.getElementById ("result").innerHTML = parseInt(num1) - parseInt(num2);
+}
+
+
+
